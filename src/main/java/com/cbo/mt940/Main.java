f9b57651fff8f064b277bc3c2d753daa7269c450
EtanaAlemu <etanaalemunew@gmail.com>
2023-11-19 20:56:03 -0800

    test

--- src/main/java/com/cbo/mt940/Main.java
+++ src/main/java/com/cbo/mt940/Main.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        buildMT940Message();
-        parseMT940Message();
+        buildMT940Message();
+//        parseMT940Message();
     }
     public static void buildMT940Message(){
 
@@ -17,14 +17,14 @@ public class Main {
         mt940Message.setTransactionReferenceNumber("20231115-319");
         mt940Message.setAccountIdentification("1022200171931");
         mt940Message.setStatementNumberSequenceNumber("319/1");
-        mt940Message.setOpeningBalance("C","231115","ETB","6,994,609.00");
+        mt940Message.setOpeningBalance("C","231115","ETB","6994609,00");
 
         mt940Message.setStatementLines("231115","1115","D","B","533408,00","NMSC","NONREF","FT23319KHPN5","Transfer");
         mt940Message.setStatementLines("231115","1115","D","B","533408,00","NMSC","NONREF","FT23319KHPN5","Transfer");
 
         // Set values for closing balance tags
-        mt940Message.setClosingBalanceBookedFunds("C", "231115", "ETB", "6,461,201.00");
-        mt940Message.setClosingAvailableBalance("C", "231115", "ETB", "6,461,201.00");
+        mt940Message.setClosingBalanceBookedFunds("C", "231115", "ETB", "6461201,00");
+        mt940Message.setClosingAvailableBalance("C", "231115", "ETB", "6461201,00");
 
         // Set header and trailer values
         mt940Message.setHeader("{1:F01CBORETAAXXXX22061ZFPHG}{2:I940XXXXXXXXXXXXN}{3:{108:22061ZFPHG97870}}{4:");
